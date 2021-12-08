8a6d27e0a81f5d49c37020f4665e9686a2aa0b70
Ashutosh Kushwaha <ashutoshkushwaha81@gmail.com>
2021-12-08 08:14:24 +0530

    Added Index Routes

---
+++ routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+func initializeRoutes(){
+	// setup index routes
+	router.GET("/", showIndexPage)
+}
+
+func showIndexPage(c *gin.Context){
+	c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Home Page",
+	})
+}
